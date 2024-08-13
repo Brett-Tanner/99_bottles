1ee8bfcea93f33245f156a5a1d8a0604aeb4f270
Brett <bretttanner171@gmail.com>
2024-08-13 12:21:51 +0900

    Refactor out '2' case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,16 +11,11 @@ class Bottles
       "1 bottle of beer.\n" \
       "Take it down and pass it around, " \
       "no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, " \
-      "2 bottles of beer.\n" \
-      "Take one down and pass it around, " \
-      "1 bottle of beer on the wall.\n"
     else
-      "#{n} bottles of beer on the wall, " \
-      "#{n} bottles of beer.\n" \
+      "#{n} #{container(n)} of beer on the wall, " \
+      "#{n} #{container(n)} of beer.\n" \
       "Take one down and pass it around, " \
-      "#{n - 1} bottles of beer on the wall.\n"
+      "#{n - 1} #{container(n - 1)} of beer on the wall.\n"
     end
   end
 
@@ -30,5 +25,11 @@ class Bottles
 
   def song
     verses(99, 0)
+  end
+
+  private
+
+  def container(n)
+    n == 1 ? "bottle" : "bottles"
   end
 end
