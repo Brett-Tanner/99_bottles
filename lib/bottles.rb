aaee2940b8f69d03a0bf2143e1f7807cf4d82f48
Brett Tanner <bretttanner171@gmail.com>
2024-08-26 20:54:41 +0900

    Rename Bottles to CountdownSong

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,14 @@
 require_relative './bottle_verse'
 
-class Bottles
+class CountdownSong
   attr_reader :verse_template
 
   def initialize(verse_template: BottleVerse)
     @verse_template = verse_template
+  end
+
+  def song
+    verses(99, 0)
   end
 
   def verse(n)
@@ -13,9 +17,5 @@ class Bottles
 
   def verses(start_verse, end_verse)
     start_verse.downto(end_verse).map { |n| verse(n) }.join("\n")
-  end
-
-  def song
-    verses(99, 0)
   end
 end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 require_relative './bottle_verse_test'
 
-class BottlesTest < Minitest::Test
+class CountdownSongTest < Minitest::Test
   def test_a_couple_verses
     expected =
       "99 bottles of beer on the wall, " +
@@ -16,7 +16,7 @@ class BottlesTest < Minitest::Test
       "98 bottles of beer.\n" +
       "Take one down and pass it around, " +
       "97 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verses(99, 98)
+    assert_equal expected, CountdownSong.new.verses(99, 98)
   end
 
   def test_a_few_verses
@@ -35,7 +35,7 @@ class BottlesTest < Minitest::Test
       "no more bottles of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
-    assert_equal expected, Bottles.new.verses(2, 0)
+    assert_equal expected, CountdownSong.new.verses(2, 0)
   end
 
   def test_the_whole_song
@@ -340,6 +340,6 @@ class BottlesTest < Minitest::Test
       No more bottles of beer on the wall, no more bottles of beer.
       Go to the store and buy some more, 99 bottles of beer on the wall.
     SONG
-    assert_equal expected, Bottles.new.song
+    assert_equal expected, CountdownSong.new.song
   end
 end
