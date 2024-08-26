bd27da00a2e9432645107eb5cf0e95a97389af4f
Brett Tanner <bretttanner171@gmail.com>
2024-08-26 22:18:34 +0900

    Use VerseFake to test CountdownSong

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -4,38 +4,26 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 require_relative './bottle_verse_test'
 
+class VerseFake
+  def self.lyrics(n)
+    "This is verse #{n}."
+  end
+end
+
 class CountdownSongTest < Minitest::Test
-  def test_a_couple_verses
-    expected =
-      "99 bottles of beer on the wall, " +
-      "99 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "98 bottles of beer on the wall.\n" +
-      "\n" +
-      "98 bottles of beer on the wall, " +
-      "98 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "97 bottles of beer on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(99, 98)
+  def test_a_single_verse
+    expected = 'This is verse 500.'
+    assert_equal expected,
+                 CountdownSong.new(verse_template: VerseFake).verse(500)
   end
 
-  def test_a_few_verses
+  def test_verses
     expected =
-      "2 bottles of beer on the wall, " +
-      "2 bottles of beer.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of beer on the wall.\n" +
-      "\n" +
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of beer on the wall.\n" +
-      "\n" +
-      "No more bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of beer on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(2, 0)
+      "This is verse 99.\n" +
+      "This is verse 98.\n" +
+      "This is verse 97."
+    assert_equal expected,
+                 CountdownSong.new(verse_template: VerseFake).verses(99, 97)
   end
 
   def test_the_whole_song
